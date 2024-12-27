678f87e38fdf7546a73480883a84384efa3f9b3e
Keegan <keeganpatton@gmail.com>
2024-12-26 20:45:40 -0600

    update dir struct | add tmpls

--- main.go
+++ main.go
@@ -9,20 +9,20 @@ import (
 
 
 /***********************************
-				Page Routing 
+            Page Routing 
 ***********************************/
 func mainPageHandler(w http.ResponseWriter, r *http.Request) {
-	 tmpl, _ := template.ParseFiles("./web/templates/index.html")
+	 tmpl, _ := template.ParseFiles("src/templates/index.html")
 	 tmpl.Execute(w, nil)
 }
 
 func loginPageHandler(w http.ResponseWriter, r *http.Request) {
-	 tmpl, _ := template.ParseFiles( "./web/templates/login.html")
+	 tmpl, _ := template.ParseFiles( "src/templates/login.html")
 	 tmpl.Execute(w, nil)
 }
 
 func storiesHandler(w http.ResponseWriter, r *http.Request) {
-	 tmpl, _ := template.ParseFiles("./web/templates/stories.html")
+	 tmpl, _ := template.ParseFiles("src/templates/stories.html")
 	 tmpl.Execute(w, nil)
 }
 
@@ -31,17 +31,14 @@ func storiesHandler(w http.ResponseWriter, r *http.Request) {
           Serves the App 
 ***********************************/
 func main() {
+	 fs := http.FileServer(http.Dir("src"))
+	 http.Handle("/src/", http.StripPrefix("/src/", fs))
 
-	 fs := http.FileServer(http.Dir("./web/static"))
-	 http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-
-	 http.HandleFunc("/", mainPageHandler)
+	 http.HandleFunc("/main", mainPageHandler)
 	 http.HandleFunc("/login", loginPageHandler)
 	 http.HandleFunc("/stories", storiesHandler)
 
 	 log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
 
 
